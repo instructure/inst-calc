4750ef263c4df86e3734af8e2264bcf58bcd5ad9
Jonathan Holt <jholt@instructure.com>
2019-07-10 14:27:48 -0500

    basic calculator layout

--- src/BasicCalculator/components/CalculatorButton.js
+++ src/BasicCalculator/components/CalculatorButton.js
@@ -1,5 +1,30 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 - present Instructure, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import React from 'react'
 import { Button } from '@instructure/ui-buttons/lib/Button'
+import { Flex } from '@instructure/ui-layout/lib/Flex'
 
 class CalculatorButton extends Button {
     constructor(props) {
@@ -8,11 +33,18 @@ class CalculatorButton extends Button {
 
     render() {
         return (
-            <Button
-                onClick={() => this.props.clickFunction(this.props.displaySymbol, this.props.hiddenSymbol)}
+            <Flex.Item
+                grow
+                width="48px"
+                padding="xxx-small"
             >
-                {this.props.displaySymbol}
-            </Button>
+                <Button
+                    onClick={() => this.props.clickFunction(this.props.displaySymbol, this.props.hiddenSymbol)}
+                    {...this.props}
+                >
+                    {this.props.displaySymbol}
+                </Button>
+            </Flex.Item>
         )
     }
 }
--- src/BasicCalculator/index.js
+++ src/BasicCalculator/index.js
@@ -31,6 +31,7 @@ import { Button } from '@instructure/ui-buttons/lib/Button'
 import CalculatorButton from './components/CalculatorButton'
 import CalculatorDisplayText from './components/CalculatorDisplayText'
 import { Grid } from '@instructure/ui-layout/lib/Grid'
+import { Flex } from '@instructure/ui-layout/lib/Flex'
 
 // import PropTypes from 'prop-types'
 
@@ -85,170 +86,219 @@ export default class Basic extends Component {
     })
   }
 
-  render () {
+  renderClearAndDisplay() {
     return (
-      <div>
-        <Button icon={IconCalculatorLine} >
-          {/* TODO: i18n everything */}
-          <ScreenReaderContent>Open the basic calculator</ScreenReaderContent>
-          Basic
-        </Button>
-        <hr />
-        <Grid>
-          <Grid.Row>
-            <Grid.Col width={1}>
-              <CalculatorButton
-                fluidWidth
-                displaySymbol="C"
-                hiddenSymbol="C"
-                clickFunction={this.clearDisplay}
-              />
-            </Grid.Col>
-            <Grid.Col width={3}>
-              <CalculatorDisplayText
-                displayText={this.state.displayString}
-              />
-            </Grid.Col>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Col width={1}>
-              <CalculatorButton
-                fluidWidth
-                displaySymbol="7"
-                hiddenSymbol="7"
-                clickFunction={this.addSymbolToDisplay}
-              />
-            </Grid.Col>
-            <Grid.Col width={1}>
-              <CalculatorButton
-                fluidWidth
-                displaySymbol="8"
-                hiddenSymbol="8"
-                clickFunction={this.addSymbolToDisplay}
-              />
-            </Grid.Col>
-            <Grid.Col width={1}>
-              <CalculatorButton
-                fluidWidth
-                displaySymbol="9"
-                hiddenSymbol="9"
-                clickFunction={this.addSymbolToDisplay}
-              />
-            </Grid.Col>
-            <Grid.Col width={1}>
-              <CalculatorButton
-                fluidWidth
-                displaySymbol="÷"
-                hiddenSymbol="/"
-                clickFunction={this.addSymbolToDisplay}
-              />
-              </Grid.Col>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Col width={1}>
-                <CalculatorButton
-                  fluidWidth
-                  displaySymbol="4"
-                  hiddenSymbol="4"
-                  clickFunction={this.addSymbolToDisplay}
-                />
-              </Grid.Col>
-              <Grid.Col width={1}>
-                <CalculatorButton
-                  fluidWidth
-                  displaySymbol="5"
-                  hiddenSymbol="5"
-                  clickFunction={this.addSymbolToDisplay}
-                />
-              </Grid.Col>
-              <Grid.Col width={1}>
-                <CalculatorButton
-                  fluidWidth
-                  displaySymbol="6"
-                  hiddenSymbol="6"
-                  clickFunction={this.addSymbolToDisplay}
-                />
-              </Grid.Col>
-              <Grid.Col width={1}>
-                <CalculatorButton
-                  fluidWidth
-                  displaySymbol="×"
-                  hiddenSymbol="*"
-                  clickFunction={this.addSymbolToDisplay}
-                />
-            </Grid.Col>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Col width={1}>
-              <CalculatorButton
-                fluidWidth
-                displaySymbol="1"
-                hiddenSymbol="1"
-                clickFunction={this.addSymbolToDisplay}
-              />
-            </Grid.Col>
-            <Grid.Col width={1}>
-              <CalculatorButton
-                fluidWidth
-                displaySymbol="2"
-                hiddenSymbol="2"
-                clickFunction={this.addSymbolToDisplay}
-              />
-            </Grid.Col>
-            <Grid.Col width={1}>
-              <CalculatorButton
-                fluidWidth
-                displaySymbol="3"
-                hiddenSymbol="3"
-                clickFunction={this.addSymbolToDisplay}
-              />
-            </Grid.Col>
-            <Grid.Col width={1}>
-              <CalculatorButton
-                fluidWidth
-                displaySymbol="-"
-                hiddenSymbol="-"
-                clickFunction={this.addSymbolToDisplay}
-              />
-            </Grid.Col>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Col width={1}>
-              <CalculatorButton
-                fluidWidth
-                displaySymbol="0"
-                hiddenSymbol="0"
-                clickFunction={this.addSymbolToDisplay}
-              />
-            </Grid.Col>
-            <Grid.Col width={1}>
-              <CalculatorButton
-                fluidWidth
-                displaySymbol="."
-                hiddenSymbol="."
-                clickFunction={this.addSymbolToDisplay}
-              />
-            </Grid.Col>
-            <Grid.Col width={1}>
-              <CalculatorButton
-                fluidWidth
-                displaySymbol="="
-                hiddenSymbol="="
-                clickFunction={this.evaluateDisplay}
-              />
-            </Grid.Col>
-            <Grid.Col width={1}>
-              <CalculatorButton
-                fluidWidth
-                displaySymbol="+"
-                hiddenSymbol="+"
-                clickFunction={this.addSymbolToDisplay}
-              />
-            </Grid.Col>
-          </Grid.Row>
-        </Grid>
-        <div>{this.state.hiddenString}</div>
-      </div>
+      <Flex.Item
+        padding="xxx-small"
+      >
+        <Flex direction="row">
+            <CalculatorButton
+              fluidWidth
+              displaySymbol="C"
+              hiddenSymbol="C"
+              clickFunction={this.clearDisplay}
+            />
+          <Flex.Item
+            size="75%"
+          >
+            <CalculatorDisplayText
+              displayText={this.state.displayString}
+            />
+          </Flex.Item>
+        </Flex>
+      </Flex.Item>
+    )
+  }
+
+  renderRow1() {
+    return (
+      <Flex.Item
+        padding="xxx-small"
+      >
+        <Flex direction="row">
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="7"
+            hiddenSymbol="7"
+            clickFunction={this.addSymbolToDisplay}
+          />
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="8"
+            hiddenSymbol="8"
+            clickFunction={this.addSymbolToDisplay}
+          />
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="9"
+            hiddenSymbol="9"
+            clickFunction={this.addSymbolToDisplay}
+          />
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="÷"
+            hiddenSymbol="/"
+            clickFunction={this.addSymbolToDisplay}
+          />
+        </Flex>
+      </Flex.Item>
+    )
+  }
+
+  renderRow2() {
+    return (
+      <Flex.Item
+        padding="xxx-small"
+      >
+        <Flex direction="row">
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="4"
+            hiddenSymbol="4"
+            clickFunction={this.addSymbolToDisplay}
+          />
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="5"
+            hiddenSymbol="5"
+            clickFunction={this.addSymbolToDisplay}
+          />
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="6"
+            hiddenSymbol="6"
+            clickFunction={this.addSymbolToDisplay}
+          />
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="×"
+            hiddenSymbol="*"
+            clickFunction={this.addSymbolToDisplay}
+          />
+        </Flex>
+      </Flex.Item>
+    )
+  }
+
+  renderRow3() {
+    return (
+      <Flex.Item
+        padding="xxx-small"
+      >
+        <Flex direction="row">
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="1"
+            hiddenSymbol="1"
+            clickFunction={this.addSymbolToDisplay}
+          />
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="2"
+            hiddenSymbol="2"
+            clickFunction={this.addSymbolToDisplay}
+          />
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="3"
+            hiddenSymbol="3"
+            clickFunction={this.addSymbolToDisplay}
+          />
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="-"
+            hiddenSymbol="-"
+            clickFunction={this.addSymbolToDisplay}
+          />
+        </Flex>
+      </Flex.Item>
+    )
+  }
+
+  renderRow4() {
+    return (
+      <Flex.Item
+        padding="xxx-small"
+      >
+        <Flex direction="row">
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="0"
+            hiddenSymbol="0"
+            clickFunction={this.addSymbolToDisplay}
+          />
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="."
+            hiddenSymbol="."
+            clickFunction={this.addSymbolToDisplay}
+          />
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="="
+            hiddenSymbol="="
+            clickFunction={this.addSymbolToDisplay}
+          />
+          <CalculatorButton
+            fluidWidth
+            displaySymbol="+"
+            hiddenSymbol="+"
+            clickFunction={this.addSymbolToDisplay}
+          />
+        </Flex>
+      </Flex.Item>
+    )
+  }
+
+  render() {
+    return (
+      <Flex
+        visualDebug
+        height="300px"
+        width="250px"
+        direction="column"
+      >
+        {this.renderClearAndDisplay()}
+        {this.renderRow1()}
+        {this.renderRow2()}
+        {this.renderRow3()}
+        {this.renderRow4()}
+        {/*
+        <Grid.Row>
+          <Grid.Col width={1}>
+            <CalculatorButton
+              fluidWidth
+              displaySymbol="0"
+              hiddenSymbol="0"
+              clickFunction={this.addSymbolToDisplay}
+            />
+          </Grid.Col>
+          <Grid.Col width={1}>
+            <CalculatorButton
+              fluidWidth
+              displaySymbol="."
+              hiddenSymbol="."
+              clickFunction={this.addSymbolToDisplay}
+            />
+          </Grid.Col>
+          <Grid.Col width={1}>
+            <CalculatorButton
+              fluidWidth
+              displaySymbol="="
+              hiddenSymbol="="
+              clickFunction={this.evaluateDisplay}
+            />
+          </Grid.Col>
+          <Grid.Col width={1}>
+            <CalculatorButton
+              fluidWidth
+              displaySymbol="+"
+              hiddenSymbol="+"
+              clickFunction={this.addSymbolToDisplay}
+            />
+          </Grid.Col>
+        </Grid.Row>*/}
+      </Flex>
     )
   }
 }
